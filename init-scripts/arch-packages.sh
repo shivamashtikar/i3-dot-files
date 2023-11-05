3b58d3552ca1c043aa2251280a393c7c88201512
Shivam Ashtikar <lightningshivam@gmail.com>
2023-11-05 20:59:38 +0530

    some changes

--- init-scripts/arch-packages.sh
+++ init-scripts/arch-packages.sh
@@ -1,1 +1,8 @@
 sudo pacman -S tmux neovim kitty yay ranger ripgrep figlet lolcat base-devel htop neofetch pass dmenu rofi okular kdenlive virtualbox jq picom wmctrl vlc youtube-dl noto-fonts-emoji
+
+yay -S \
+  android-studio\
+  spotify\
+  google-chrome\
+  otf-fira-code-git
+pacman -S obs-studio
