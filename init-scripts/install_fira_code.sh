57a81644e9562a7e44b4bd2c1f9d2012dd28a646
Shivam Ashtikar <lightningshivam@gmail.com>
2024-01-21 02:58:20 +0530

    updated feracode downlaod scrip

--- init-scripts/install_fira_code.sh
+++ init-scripts/install_fira_code.sh
@@ -1,7 +1,18 @@
-mkdir -p ~/.local/share/fonts
-touch download.sh
-for type in Bold Light Medium Regular Retina; do
-  wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
-    "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
-done
+#!/usr/bin/env bash
+
+fonts_dir="${HOME}/.local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+else
+    echo "Found fonts dir $fonts_dir"
+fi
+
+version=5.2
+zip=Fira_Code_v${version}.zip
+curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+unzip -o -q -d ${fonts_dir} ${zip}
+rm ${zip}
+
+echo "fc-cache -f"
 fc-cache -f
